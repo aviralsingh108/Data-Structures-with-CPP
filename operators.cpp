ee6162c509327b1da6a65bd789e7dc4defd50920
aviralsingh108 <admiralaviral@gmail.com>
2023-03-25 13:44:38 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a = 2 / 5;
+    cout << a << endl;
+
+    float b;
+    b = 2.0 / 5;
+    cout << b << endl;
+
+    double c;
+    c = 2.0 / 5;
+    cout << c << endl;
+}
