61db67484bd6e8fa2269d870f45750956ec16bec
aviralsingh108 <admiralaviral@gmail.com>
2023-03-25 10:30:14 +0530

    type casting

---
+++ typeCasting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        // Type Casting
+        int a = 'a';
+        cout << a << endl;
+        char b = 97;
+        cout << b << endl;
+        char c = 123456;
+        cout << c << endl;
+}
