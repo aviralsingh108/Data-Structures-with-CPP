9c17297b01a56a65f8faf7c145c945fc716d4076
aviralsingh108 <admiralaviral@gmail.com>
2023-04-04 16:46:58 +0530

    prime no

--- conditionalsAndLoops.cpp
+++ conditionalsAndLoops.cpp
@@ -399,18 +399,19 @@ int main()
     //     }
     // }
 
-    int n, flag = 0;
-    cin >> n;
+    // Prime No
+    // int n, flag = 0;
+    // cin >> n;
 
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
-            cout << "Not Prime";
-            flag = 1;
-            break;
-        }
-    }
-    if (flag == 0)
-        cout << "Prime";
+    // for (int i = 2; i < n; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << "Not Prime";
+    //         flag = 1;
+    //         break;
+    //     }
+    // }
+    // if (flag == 0)
+    //     cout << "Prime";
 }
