2d006c6c0c7d6afe262a050d8268e3208f3f14ee
aviralsingh108 <admiralaviral@gmail.com>
2023-04-09 17:02:06 +0530

    Binary to Decimal

--- conditionalsAndLoops.cpp
+++ conditionalsAndLoops.cpp
@@ -417,13 +417,26 @@ int main()
     //     cout << "Prime";
 
     // Decimal to Binary
+    // int n, ans = 0, i = 0;
+    // cin >> n;
+    // while (n != 0)
+    // {
+    //     int bit = n & 1;
+    //     ans = (bit * pow(10, i) + ans);
+    //     n = n >> 1;
+    //     i++;
+    // }
+    // cout << ans;
+
+    // Binary to Decimal
     int n, ans = 0, i = 0;
     cin >> n;
     while (n != 0)
     {
-        int bit = n & 1;
-        ans = (bit * pow(10, i) + ans);
-        n = n >> 1;
+        int digit = n % 10;
+        if (digit == 1)
+            ans += pow(2, i);
+        n = n / 10;
         i++;
     }
     cout << ans;
