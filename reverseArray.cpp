feb85888fccf074c6299a5bfa7dd547813623cb1
aviralsingh108 <admiralaviral@gmail.com>
2023-05-20 12:07:31 +0530

    reverse array

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 void reverseArray(int arr[], int size)
 {
-
-    for (int i = 0; i < size / 2; i++)
+    int start = 0, end = size - 1;
+    while (start <= end)
     {
-        swap(arr[i], arr[size - i - 1]);
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
     }
 }
 
